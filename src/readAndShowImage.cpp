3c2bad3114e5c43b5eabe23b55247e964b6da003
HetDaftary <hetdaftary@gmail.com>
2021-10-27 21:37:09 +0530

    Adding code to tutorial to read and show an image

--- src/readAndShowImage.cpp
+++ src/readAndShowImage.cpp
@@ -5,15 +5,15 @@ using namespace cv;
 using namespace std;
 
 int main(int argc, char** argv){
-  cv::Mat img;
-  img = cv::imread("img/lena.jpg", cv::IMREAD_COLOR);
+    cv::Mat img;
+    img = cv::imread("img/lena.jpg", cv::IMREAD_COLOR);
   
-  if(!img.data){
-    std::cout << "Image not found" << std::endl;
-    return -1;
-  }
+    if(!img.data){
+        std::cout << "Image not found" << std::endl;
+        return -1;
+    }
   
-  cv::imshow("Lena", img);
-  cv::waitKey(10000);
-  cv::destroyAllWindows();
+    cv::imshow("Lena", img);
+    cv::waitKey(10000);
+    cv::destroyAllWindows();
 }
