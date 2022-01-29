11551286a63e0079ca734fd57e979d745dc892d3
Het Daftary <51186433+HetDaftary@users.noreply.github.com>
2022-01-29 15:22:58 +0530

    Update writeImage.cpp

--- src/writeImage.cpp
+++ src/writeImage.cpp
@@ -8,7 +8,7 @@ using namespace std;
 int main() {
     cv::Mat img = cv::imread("data/input/images/lena.jpg");
     // If path where result file is stored does not exist, we make a new folder for the path.
-    system("if [ ! -f data/Results/writeImage/ ]; then mkdir -p data/Results/writeImage/; fi");
+    system("if [ ! -d data/Results/writeImage/ ]; then mkdir -p data/Results/writeImage/; fi");
     cv::imwrite("data/Results/writeImage/lena.jpg", img);
     return 0;
 }
