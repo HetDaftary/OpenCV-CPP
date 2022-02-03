5790d616316aed8e2a81b8122790ff30abe650a8
Het Daftary <51186433+HetDaftary@users.noreply.github.com>
2022-02-03 14:31:39 +0530

    Update Install-OpenCV.sh

--- scripts/Install-OpenCV.sh
+++ scripts/Install-OpenCV.sh
@@ -6,14 +6,9 @@ sudo git clone https://github.com/opencv/opencv_contrib.git
 cd opencv
 sudo mkdir release
 cd release
-sudo cmake -D BUILD_TIFF=ON -D WITH_CUDA=OFF -D ENABLE_AVX=OFF -D WITH_OPENGL=OFF -D WITH_OPENCL=OFF -D WITH_IPP=OFF -D WITH_TBB=ON -D BUILD_TBB=ON -D WITH_EIGEN=OFF -D WITH_V4L=OFF -D WITH_VTK=OFF -D BUILD_TESTS=OFF -D BUILD_PERF_TESTS=OFF -D OPENCV_GENERATE_PKGCONFIG=ON -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D OPENCV_EXTRA_MODULES_PATH=/opt/opencv_contrib/modules /opt/opencv/
+sudo cmake -D BUILD_TIFF=ON -D WITH_CUDA=ON -D ENABLE_AVX=OFF -D WITH_OPENGL=ON -D WITH_OPENCL=ON -D WITH_IPP=OFF -D WITH_TBB=ON -D BUILD_TBB=ON -D WITH_EIGEN=ON -D WITH_V4L=OFF -D WITH_VTK=OFF -D BUILD_TESTS=OFF -D BUILD_PERF_TESTS=OFF -D OPENCV_GENERATE_PKGCONFIG=ON -D CMAKE_BUILD_TYPE=RELEASE -D WITH_QT=ON -D CMAKE_INSTALL_PREFIX=/usr/local -D OPENCV_EXTRA_MODULES_PATH=/opt/opencv_contrib/modules /opt/opencv/
 sudo make -j8
 sudo make install
 sudo ldconfig
 cd ~
-
-if [ ! -d /usr/local/lib/pkgconfig ]; then 
-  sudo mkdir /usr/local/lib/pkgconfig
-fi
-
 sudo cp /usr/local/lib/pkgconfig/opencv4.pc /usr/lib/x86_64-linux-gnu/pkgconfig/opencv.pc
